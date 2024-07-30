4e002f9f3163a817952b85e50488abe87ec0b628
amirKhani <ww.amirkhani.i@gmail.com>
2024-07-30 11:39:31 +0330

    create layout

---
+++ components/layout/Layout.js
@@ -0,0 +1,24 @@
+import Link from "next/link";
+import classes from "./layout.module.css";
+
+function Layout({ children }) {
+  return (
+    <>
+      <header className={classes.header}>
+        <div className={classes.left}>
+          <Link href="/">KHANIFOOD</Link>
+        </div>
+        <div className={classes.right}>
+          <Link href="/menu">menu</Link>
+          <Link href="/categories">categories</Link>
+        </div>
+      </header>
+      <div className={classes.container}>{children}</div>
+      <footer className={classes.footer}>
+        <p>Developed by Amir Khani with ❤️ || Next.js</p>
+      </footer>
+    </>
+  );
+}
+
+export default Layout;
