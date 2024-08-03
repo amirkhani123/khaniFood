fd4a3d68821421a2cf83e93b66bcdb21f80f6a00
amirKhani <ww.amirkhani.i@gmail.com>
2024-08-03 13:15:45 +0330

    crete noutFoundPage

---
+++ pages/404.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+function NotFoundPage() {
+  return (
+    <div
+      style={{
+        maxWidth: "1100px",
+        margin: " 35px auto",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "column",
+      }}
+    >
+      <img src="/images/notFound.png" alt="notFoundImage" style={{width:"50%"}}/>
+      <h1 style={{ fontSize: "1.2rem",fontWeight:"500",color:"red" }}>NOT FOUND PAGE !!🫠</h1>
+    </div>
+  );
+}
+
+export default NotFoundPage;
